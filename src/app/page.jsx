18260f9f12cce85f7cab7283e98cf8a8abd97992
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-27 16:34:03 +0530

    writes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
-import Navbar from "@/components/navbar/Navbar";
+
 import styles from "./homepage.module.css";
-import Footer from "@/components/footer/Footer";
+
 
 export default function Home() {
   return <div>
-    <Navbar/>
-    <Footer/>
+   
     </div>;
 }
