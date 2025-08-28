2ec33738004309982806ecea06d32ff234673fe2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-28 22:11:36 +0530

    Blog

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
+import { ThemeContextProvider } from '@/context/ThemeContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={inter.className}>
+        <ThemeContextProvider>
         <div className="container">
           <div className='wrapper'>
             <Navbar/>
@@ -21,6 +24,7 @@ export default function RootLayout({ children }) {
             <Footer/>
           </div>
         </div>
+        </ThemeContextProvider>
       </body>
     </html>
   )
